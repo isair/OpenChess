38412ca28e2eba14d64828a3d2136f448b3cc7e1
Baris Sencan <run32a50mch@gmail.com>
2013-07-30 03:43:39 +0300

    Resolution fix for the HTML project.

--- open-chess-html/src/com/bsencan/openchess/client/GwtLauncher.java
+++ open-chess-html/src/com/bsencan/openchess/client/GwtLauncher.java
@@ -1,19 +1,20 @@
 package com.bsencan.openchess.client;
 
-import com.bsencan.openchess.OpenChess;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.bsencan.openchess.OpenChess;
 
 public class GwtLauncher extends GwtApplication {
 	@Override
-	public GwtApplicationConfiguration getConfig () {
-		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+	public GwtApplicationConfiguration getConfig() {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(540,
+				960);
 		return cfg;
 	}
 
 	@Override
-	public ApplicationListener getApplicationListener () {
+	public ApplicationListener getApplicationListener() {
 		return new OpenChess();
 	}
 }
